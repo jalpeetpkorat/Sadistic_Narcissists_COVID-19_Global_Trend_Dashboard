eaffdb7c54045885b4aa1a0bc8c63ffcc8afcce8
Jalpeet Korat <75266434+jalpeetpkorat@users.noreply.github.com>
2024-12-18 00:16:36 +0530

    Update callbacks.py

--- callbacks.py
+++ callbacks.py
@@ -1,67 +1,42 @@
-# callbacks.py
-
 import plotly.graph_objects as go
 import plotly.express as px
 from dash.dependencies import Input, Output
-from data import load_data
+from data_processing import load_data, get_country_summary
 
 # Load data
-country_data, confirmed_data, deaths_data, vaccination_data = load_data()
+confirmed_data, deaths_data, vaccination_data = load_data()
+country_data = get_country_summary(confirmed_data, deaths_data, vaccination_data)
 
-# Define the callbacks for updating the map and time-series graph
 def register_callbacks(app):
-
     @app.callback(
-        [Output("map-graph", "figure"),
-         Output("time-series-graph", "figure")],
-        [Input("country-dropdown", "value"),
-         Input("data-dropdown", "value")]
+        [Output("map-graph", "figure"), Output("time-series-graph", "figure")],
+        [Input("country-dropdown", "value"), Input("data-dropdown", "value")]
     )
-    def update_map_and_graph(selected_country, selected_data):
-        if selected_data == "Confirmed Cases":
-            color_column = "Confirmed Cases"
-            title_map = f"COVID-19 Confirmed Cases in {selected_country}"
-            title_graph = f"COVID-19 Confirmed Cases Over Time for {selected_country}"
-        elif selected_data == "Deaths":
-            color_column = "Deaths"
-            title_map = f"COVID-19 Deaths in {selected_country}"
-            title_graph = f"COVID-19 Deaths Over Time for {selected_country}"
-        else:
-            color_column = "Vaccinations"
-            title_map = f"COVID-19 Vaccinations in {selected_country}"
-            title_graph = f"COVID-19 Vaccinations Over Time for {selected_country}"
+    def update_visualizations(selected_country, selected_data):
+        # Choropleth map
+        map_figure = go.Figure(go.Choropleth(
+            locations=country_data["Country"],
+            locationmode="country names",
+            z=country_data[selected_data],
+            colorscale="Blues",
+            colorbar_title=selected_data
+        ))
+        map_figure.update_layout(title=f"Global {selected_data} Data")
 
-        selected_data_df = country_data[country_data["Country"] == selected_country]
-        
-        # Map Plotly
-        map_fig = go.Figure(
-            data=go.Choropleth(
-                locations=selected_data_df["Country"],
-                z=selected_data_df[color_column],
-                hoverinfo="location+z",
-                colorscale="Viridis"
-            )
-        )
-        map_fig.update_layout(title=title_map, geo=dict(showcoastlines=True))
-        
-        # Time-series Plotly
-        if selected_data == "Confirmed Cases":
-            data_df = confirmed_data[confirmed_data["Country"] == selected_country]
-        elif selected_data == "Deaths":
-            data_df = deaths_data[deaths_data["Country"] == selected_country]
+        # Time-series graph
+        if selected_data == "total_vaccinations":
+            ts_data = vaccination_data[vaccination_data["Country"] == selected_country]
+        elif selected_data == "Confirmed Cases":
+            ts_data = confirmed_data[confirmed_data["Country/Region"] == selected_country]
         else:
-            data_df = vaccination_data[vaccination_data["location"] == selected_country]
-        
-        time_series_fig = px.line(
-            data_df,
-            x="Date",
-            y=color_column,
-            title=title_graph
-        )
-        time_series_fig.update_layout(
-            xaxis_title="Date",
-            yaxis_title=selected_data,
-            showlegend=False
-        )
-        
-        return map_fig, time_series_fig
+            ts_data = deaths_data[deaths_data["Country/Region"] == selected_country]
+
+        if ts_data.empty:
+            time_series_figure = go.Figure()
+            time_series_figure.update_layout(title=f"No {selected_data} Data Available")
+        else:
+            time_series_figure = px.line(
+                ts_data, x="Date", y=selected_data,
+                title=f"{selected_data} Over Time in {selected_country}"
+            )
+        return map_figure, time_series_figure
