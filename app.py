db8a615d2e479cfc440d88d96b1064d33c42a9fc
Jalpeet Korat <75266434+jalpeetpkorat@users.noreply.github.com>
2024-12-18 00:16:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
-# app.py
-
-import dash
-from layout import app_layout
+from dash import Dash
+from visualization import layout
 from callbacks import register_callbacks
 
-# Initialize the Dash app
-app = dash.Dash(__name__)
+# Initialize Dash app
+app = Dash(__name__)
+app.layout = layout()
 
-# Set the app layout
-app.layout = app_layout
-
-# Register the callbacks
+# Register callbacks
 register_callbacks(app)
 
-# Run the app
+# Run the server
 if __name__ == "__main__":
     app.run_server(debug=True)
